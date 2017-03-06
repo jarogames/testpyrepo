042cfe7fb0cf8a8462d9c49270bfd178c3ce6b7f
Jaromrax <jaromrax@gmail.com>
2017-03-06 18:16:46 +0100

    seems i have all basic file there

---
+++ MySuperPackage/__init__.py
@@ -0,0 +1,5 @@
+# all imports here are for all package level
+
+# import * will call __all__
+
+__all__=[]
--- setup.py
+++ setup.py
@@ -4,9 +4,13 @@ from setuptools import setup
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-
+##############
+#packages=[]   #  directory with __init__.py
+#modules=[]    #  single files in root dir
+#https://pythonhosted.org/an_example_pypi_project/setuptools.html
+#
 #https://pypi.python.org/pypi?%3Aaction=list_classifiers
+#
 setup(
     name = "testpyrepo",
     version = "0.0.1",
@@ -17,7 +21,7 @@ setup(
     license = "GPLv2",
     keywords = "example  tutorial",
     url = "http://www.seznam.cz",
-    packages=['NuPyPhy', 'tests'],
+    packages=['MySuperPackage'],
     long_description=read('README.md'),
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
